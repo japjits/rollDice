7b48b62954dd27e7713fcd7494d4436b7f941489
japjits <japjitbhatia@gmail.com>
2019-11-06 15:45:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ var images2 = "images/dice"+ randomNumber2 +".png";
 
 document.querySelector(".dice .img1").setAttribute("src",images1);
 document.querySelector(".dice .img2").setAttribute("src",images2);
+
+if(randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML = " Player 1 wins! ";
+}
+else if ( randomNumber2 > randomNumber1)
+{
+  document.querySelector("h1").innerHTML = "Player 2 wins! ";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "It's a Draw";
+}
